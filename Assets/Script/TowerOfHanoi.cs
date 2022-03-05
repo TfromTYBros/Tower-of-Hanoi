9a0f7fffd456c3a2c6de7cc19dbd3e6e93c99658
TfromTYBros <kanshashitemasu358@gmail.com>
2022-03-05 11:39:58 +0900

    Stackか？int管理か？

--- Assets/Script/TowerOfHanoi.cs
+++ Assets/Script/TowerOfHanoi.cs
@@ -5,11 +5,23 @@ using UnityEngine;
 public class TowerOfHanoi : MonoBehaviour
 {
     public GameObject[] Bases;
-    public GameObject[] Wheels;
+    public GameObject[] WheelPrefabs;
+    public GameObject PickUpWheel;
+    public GameObject WheelsParent;
+
+    public int GameLevel = 8;
+    /*
+    public int WheelsCount_Left = 0;
+    public int WheelsCount_Center = 0;
+    public int WheelsCount_Right = 0;*/
+    public Stack<GameObject> Stack_Left;
+    public Stack<GameObject> Stack_Center;
+    public Stack<GameObject> Stack_Right;
+
     // Start is called before the first frame update
     void Start()
     {
-        FFF();
+        StartGame();
     }
 
     // Update is called once per frame
@@ -18,10 +30,34 @@ public class TowerOfHanoi : MonoBehaviour
         
     }
 
-    public void FFF()
+    void StartGame()
     {
+        StartCoroutine(MakeTowerBySelectLevel(GameLevel));
+    }
+
+    IEnumerator MakeTowerBySelectLevel(int level)
+    {
+        //WheelsCount_Left = level;
+
         float yOffset = 0.5f;
-        GameObject wheel8 = Instantiate(Wheels[7], new Vector3(Bases[0].transform.position.x, Bases[0].transform.position.y+yOffset, Bases[0].transform.position.z), Quaternion.Euler(0,0,90f));
-        wheel8.name = "wheel8";
+        for(int i = level-1; i >= 0; i--)
+        {
+            yield return new WaitForSeconds(0.1f);
+            GameObject newWheel = Instantiate(WheelPrefabs[i], new Vector3(Bases[0].transform.position.x, Bases[0].transform.position.y + yOffset, Bases[0].transform.position.z), Quaternion.Euler(0, 0, 90f),WheelsParent.transform);
+            newWheel.name = "Wheel Value" + (i+1).ToString();
+            yOffset += 0.5f;
+        }
+        
+    }
+
+    void PickUp()
+    {
+        //if(WheelsCount_Left != 0)一番上のオブジェクトを空中のY座標に移動させる
+
+    }
+
+    void GameSet()
+    {
+
     }
 }
--- Assets/Script/UserImput.cs
+++ Assets/Script/UserImput.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class UserImput : MonoBehaviour
 {
+    TowerOfHanoi ToH;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ToH = FindObjectOfType<TowerOfHanoi>();
     }
 
     // Update is called once per frame
@@ -19,6 +20,8 @@ public class UserImput : MonoBehaviour
     public void PopFromLeft()
     {
         Debug.Log("PopFromLeft():");
+        //if(PickUp.empty())ˆê”Ôã‚ğPickUp‚ÉˆÚ‚·ŠÖ”()
+        //else PickUp‚©‚çLeft‚ÉˆÚ‚·ŠÖ”()
     }
 
     public void PopFromCenter()
